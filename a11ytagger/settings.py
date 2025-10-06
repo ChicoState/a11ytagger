5b5edb951ff88604c9daaf14034424075a1ac1da
Colin Jamison <Valinora@users.noreply.github.com>
2025-10-06 11:04:11 -0700

    PDF Upload MVP (#24)
    * chore: set port for dev server, add template dir
    
    * feat: add pdf dependency, and create a hello world view
    
    * fix: set DEBUG env var to false in Dockerfile
    
    * feat: add PDF upload view
    
    * feat: add multipage pdf viewing

--- a11ytagger/settings.py
+++ a11ytagger/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$c%$jwi%!xjy()pa(h&hd99uv5liyro-8po-k9p%@%f2(!wn#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
