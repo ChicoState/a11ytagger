19dea20405f1a0bc76a1d7fc0a0ce4575819afd2
Colin Jamison <colin.jamison@liatrio.com>
2025-11-07 13:49:24 -0800

    move backend to port 3000

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 def main():
-    uvicorn.run("a11ytagger.asgi:application", host="0.0.0.0", port=8080)
+    uvicorn.run("a11ytagger.asgi:application", host="0.0.0.0", port=3000)
 
 
 if __name__ == "__main__":
